d0bf8f009daa39cabe0702ceeb8d344d740127b8
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-05-01 22:37:09 +0800

    first commit

---
+++ src/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+    const cardComponent = robots.map(user => {
+        return <Card id={user.id} name={user.name} username={user.username} email={user.email}/>
+    });
+    return (
+        <div>
+            {cardComponent}
+        </div>
+    );
+}
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import CardList from './CardList';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Fragment>
+      <h1 className="tc">Robofriends</h1>
+    </Fragment>
+    <CardList robots={robots} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
